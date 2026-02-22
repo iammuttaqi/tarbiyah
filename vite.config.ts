af2a2ce25ce671283f9103d9a373f003bd5bf6f5
Muttaqi <muntasermuttaqi@gmail.com>
2026-02-22 23:23:27 +0600

    feat: Specify multiple HTML entry points in Vite build configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        about: "about.html",
+        contact: "contact.html"
+      }
+    }
+  }
 });
