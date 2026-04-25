a932a2e12485334efdb7533927d6b9fd873fbdaa
Muttaqi <muntasermuttaqi@gmail.com>
2026-04-26 05:12:36 +0600

    queue work add

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// long running process
+Schedule::withoutOverlapping(1440 * 30)->everyMinute()->runInBackground()->group(function () {
+    Schedule::command('queue:work -v --queue=messages,high,default --tries=3 --rest=1')
+        ->onSuccess(fn (Stringable $output) => logger()->info('Succeded Command: queue:work -v --queue=messages,high,default --tries=3 --rest=1', ['output' => $output]))
+        ->onFailure(fn (Stringable $output) => logger('Failed Command: queue:work -v --queue=messages,high,default --tries=3 --rest=1', ['output' => $output]));
+});
